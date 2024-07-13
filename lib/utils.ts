27a069b63edec73d4e6f9c9bd12c7e7d063e0165
Micado Pierre <Biml16gb@gmail.com>
2024-07-13 03:44:49 -0400

    My Portfolio App

---
+++ lib/utils.ts
@@ -0,0 +1,10 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+
+//  8:56: 12 on video turtoial
+
